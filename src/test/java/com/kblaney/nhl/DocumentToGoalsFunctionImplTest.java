aa7dc78c01f29b5c7431f9e80b5db4dd65a63d65
Kyle Blaney <blaney.kyle@gmail.com>
2013-02-05 17:19:59 -0500

    Add more tests to DocumentToGoalsFunctionImplTest.

--- src/test/java/com/kblaney/nhl/DocumentToGoalsFunctionImplTest.java
+++ src/test/java/com/kblaney/nhl/DocumentToGoalsFunctionImplTest.java
@@ -16,10 +16,26 @@ public final class DocumentToGoalsFunctionImplTest
   }
 
   @Test
-  public void getGoals_GameNumberTwelve() throws Exception
+  public void getGoals_GameNumber12() throws Exception
   {
     int gameNum = 12;
     final Document document = Documents.getDocumentForGameNum(gameNum);
     assertEquals(6, function.getGoals(document, gameNum).size());
+  }
+
+  @Test
+  public void getGoals_GameNumber73() throws Exception
+  {
+    int gameNum = 73;
+    final Document document = Documents.getDocumentForGameNum(gameNum);
+    assertEquals(8, function.getGoals(document, gameNum).size());
+  }
+
+  @Test
+  public void getGoals_GameNumber127() throws Exception
+  {
+    int gameNum = 127;
+    final Document document = Documents.getDocumentForGameNum(gameNum);
+    assertEquals(3, function.getGoals(document, gameNum).size());
   }
 }
