ec5a412de39acbd37f02695cd5b0beac56b99ef0
Kyle Blaney <blaney.kyle@gmail.com>
2013-02-07 15:26:14 -0500

    Remove classes that get only goals and only face offs.

--- src/main/java/com/kblaney/nhl/DocumentToFaceOffsFunction.java
+++
@@ -1,9 +0,0 @@
-package com.kblaney.nhl;
-
-import java.util.List;
-import org.jsoup.nodes.Document;
-
-public interface DocumentToFaceOffsFunction
-{
-  List<FaceOff> getFaceOffs(Document document, int gameNum);
-}
--- src/main/java/com/kblaney/nhl/DocumentToGoalsFunction.java
+++
@@ -1,9 +0,0 @@
-package com.kblaney.nhl;
-
-import java.util.List;
-import org.jsoup.nodes.Document;
-
-public interface DocumentToGoalsFunction
-{
-  List<Goal> getGoals(Document document, int gameNum);
-}
