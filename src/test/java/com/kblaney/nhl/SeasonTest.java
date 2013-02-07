c60c512729f8f995d1b15f4feb01ef6dddc3c7c4
Kyle Blaney <blaney.kyle@gmail.com>
2013-02-07 17:50:17 -0500

    Add seasons dating back to 2007-2008.

--- src/test/java/com/kblaney/nhl/SeasonTest.java
+++ src/test/java/com/kblaney/nhl/SeasonTest.java
@@ -16,4 +16,28 @@ public final class SeasonTest
   {
     assertEquals("20112012/PL02", Season.REGULAR_SEASON_2011_2012.inUrl());
   }
+
+  @Test
+  public void inUrl_RegularSeason20102011()
+  {
+    assertEquals("20102011/PL02", Season.REGULAR_SEASON_2010_2011.inUrl());
+  }
+
+  @Test
+  public void inUrl_RegularSeason20092010()
+  {
+    assertEquals("20092010/PL02", Season.REGULAR_SEASON_2009_2010.inUrl());
+  }
+
+  @Test
+  public void inUrl_RegularSeason20082009()
+  {
+    assertEquals("20092010/PL02", Season.REGULAR_SEASON_2009_2010.inUrl());
+  }
+
+  @Test
+  public void inUrl_RegularSeason20072008()
+  {
+    assertEquals("20072008/PL02", Season.REGULAR_SEASON_2007_2008.inUrl());
+  }
 }
